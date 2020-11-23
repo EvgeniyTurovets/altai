8453978af5162a25da024095d0ebd3b4a6656623
sanyokstd <59456430+sanyokstd@users.noreply.github.com>
2020-11-23 17:56:46 +0200

    1
    каталог

--- src/js/main.js
+++ src/js/main.js
@@ -20,4 +20,14 @@ $(function(){
     $('.level2__close').click(function(){
       $('.mob-menu .level2').fadeOut()
     })
+
+    // каталог
+    $('.catalog__filter__title').click(function(){
+      $(this).next('.catalog__filter__wrap').slideToggle()
+      $(this).closest('.catalog__filter__item').toggleClass('active')
+    })
+
+    if( $('.select').length ){
+      $('.select').selectric()
+    }
 })
